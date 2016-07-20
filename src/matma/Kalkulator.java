74de65561d150dafd8ca8c0748d4a79db821d62d
Anna Pazera <anna.pazera@tlen.pl>
2016-07-20 12:43:50 +0200

    Dodawanie

---
+++ src/matma/Kalkulator.java
@@ -0,0 +1,16 @@
+package matma;
+
+/**
+ * Created by Anna Kacprzak on 2016-07-20.
+ */
+public class Kalkulator {
+    public static double dodawaj(double a, double b){
+        double c;
+         c= a+b;
+        return c;
+
+    }
+
+
+
+}
---
+++ src/matma/Matematyk.java
@@ -0,0 +1,11 @@
+package matma;
+
+/**
+ * Created by Anna Kacprzak on 2016-07-20.
+ */
+public class Matematyk {
+    public static void main (String [] args) {
+        double wynik = Kalkulator.dodawaj(2, 3);
+        System.out.println("Suma liczb a i b to " + wynik );
+    }
+}
